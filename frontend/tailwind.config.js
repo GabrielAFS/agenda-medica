bb0a0e461c49b7d1b2234b697158a2e3884ecfcd
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-05-20 17:34:52 -0300

    feat(frontend): Setup tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Inclua todos os arquivos do seu projeto
+    "./public/index.html", // Inclua o HTML, se necessário
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
