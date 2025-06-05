b392dcbda11823efd1aa20eb06c9670eebe32bad
Gabriel Augusto Fontes da Silva <gabriel_fsilva@outlook.com.br>
2025-06-04 22:47:56 -0300

    refactor(frontend): Add list of links and change colors

--- frontend/src/components/routes/ProtectedLayout.tsx
+++ frontend/src/components/routes/ProtectedLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { JSX } from "react";
 import { Link, useLocation } from "react-router-dom";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
@@ -7,6 +7,33 @@ import AssignmentIcon from "@mui/icons-material/Assignment";
 
 import { useAuth } from "../../hooks/useAuth";
 
+interface LinkProps {
+  to: string;
+  title: string;
+  icon: React.ReactNode;
+}
+
+const doctorLinks: LinkProps[] = [
+  {
+    to: "/agenda",
+    title: "Agenda",
+    icon: <CalendarMonthIcon fontSize='small' className='mr-2' />,
+  },
+];
+
+const pacientLinks: LinkProps[] = [
+  {
+    to: "/medicos",
+    title: "Médicos",
+    icon: <PeopleAltIcon fontSize='small' className='mr-2' />,
+  },
+  {
+    to: "/minhas-consultas",
+    title: "Consultas",
+    icon: <AssignmentIcon fontSize='small' className='mr-2' />,
+  },
+];
+
 const ProtectedLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
@@ -14,67 +41,34 @@ const ProtectedLayout: React.FC<{ children: React.ReactNode }> = ({
   const location = useLocation();
 
   const isDoctor = user && "crm" in user;
+  const links = isDoctor ? doctorLinks : pacientLinks;
 
   return (
     <div className='flex min-h-screen bg-gray-50'>
       <aside className='w-56 min-h-screen bg-gray-100 p-6 flex flex-col gap-4 border-r border-gray-200'>
-        <div className='font-extrabold text-2xl mb-8 text-blue-700'>
-          Agenda Médica
-        </div>
-        {isDoctor ? (
-          <>
+        <div className='font-extrabold text-2xl mb-8'>Agenda Médica</div>
+        <>
+          {links.map(({ to, icon, title }) => (
             <Link
-              to='/agenda'
+              to={to}
               className={`text-lg rounded px-2 py-2 transition-colors ${
-                location.pathname.startsWith("/agenda")
-                  ? "font-bold text-blue-600 bg-blue-100"
-                  : "text-gray-700 hover:bg-blue-50"
+                location.pathname.startsWith(to)
+                  ? "font-bold text-white bg-gray-400"
+                  : "text-gray-700 hover:bg-gray-50"
               }`}
             >
-              <CalendarMonthIcon fontSize='small' className='mr-2' />
-              Agenda
+              {icon}
+              {title}
             </Link>
-            <button
-              className='text-lg text-red-600 text-left px-2 py-2 rounded hover:bg-red-50 transition-colors'
-              onClick={signOut}
-            >
-              <LogoutIcon fontSize='small' className='mr-2' />
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <Link
-              to='/medicos'
-              className={`text-lg rounded px-2 py-2 transition-colors ${
-                location.pathname.startsWith("/medicos")
-                  ? "font-bold text-blue-600 bg-blue-100"
-                  : "text-gray-700 hover:bg-blue-50"
-              }`}
-            >
-              <PeopleAltIcon fontSize='small' className='mr-2' />
-              Médicos
-            </Link>
-            <Link
-              to='/minhas-consultas'
-              className={`text-lg rounded px-2 py-2 transition-colors ${
-                location.pathname.startsWith("/minhas-consultas")
-                  ? "font-bold text-blue-600 bg-blue-100"
-                  : "text-gray-700 hover:bg-blue-50"
-              }`}
-            >
-              <AssignmentIcon fontSize='small' className='mr-2' />
-              Consultas
-            </Link>
-            <button
-              className='text-lg text-red-600 text-left px-2 py-2 rounded hover:bg-red-50 transition-colors'
-              onClick={signOut}
-            >
-              <LogoutIcon fontSize='small' className='mr-2' />
-              Logout
-            </button>
-          </>
-        )}
+          ))}
+          <button
+            className='text-lg text-red-600 text-left px-2 py-2 rounded hover:bg-red-50 transition-colors'
+            onClick={signOut}
+          >
+            <LogoutIcon fontSize='small' className='mr-2' />
+            Logout
+          </button>
+        </>
       </aside>
       <main className='flex-1 min-h-screen bg-gray-50'>{children}</main>
     </div>
